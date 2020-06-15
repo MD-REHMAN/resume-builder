9860218c26b38a58fc2cf7c3d44a70e292a89afb
MD-REHMAN <Rehman.2468@gmail.com>
2020-06-15 21:04:42 +0530

    Updates

---
+++ precache-manifest.3250e5e2ab4e7d905550b2b67ada9610.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de768bb08b886b24e30d980dd9b187ba",
+    "url": "/resume-builder/index.html"
+  },
+  {
+    "revision": "b6fe36664b79792b4da9",
+    "url": "/resume-builder/static/css/main.c5003ae8.chunk.css"
+  },
+  {
+    "revision": "cc805f49e6156c8df324",
+    "url": "/resume-builder/static/js/2.d411cf6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume-builder/static/js/2.d411cf6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fe36664b79792b4da9",
+    "url": "/resume-builder/static/js/main.abc4b1fc.chunk.js"
+  },
+  {
+    "revision": "24d603423d2653365955",
+    "url": "/resume-builder/static/js/runtime-main.9c6a8a92.js"
+  },
+  {
+    "revision": "855cf5dd324c19bdd32a1032191f939b",
+    "url": "/resume-builder/static/media/ME.855cf5dd.jpg"
+  }
+]);
